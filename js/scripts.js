32ce63fe39b8bbcc71d0edef499dcd4c1124fa9c
Jeff Lai <pjefflai@gmail.com>
2021-10-25 13:32:43 -0700

    add loops simplify scripts.js code
    Co-authored-by: Christopher Neal <christopher.m.neal@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,21 @@
 jQuery(document).ready(function () {
-  jQuery("h1").click(function () {
-    alert("This is a heading.");
+  const htmlTag = ["h1", "p", "img"];
+  htmlTag.forEach(function (element) {
+    jQuery(element).click(function () {
+      alert("This is a " + element + " tag.");
+    });
   });
+  // jQuery("h1").click(function () {
+  //   alert("This is a heading.");
+  // });
 
-  jQuery("p").click(function () {
-    alert("This is a paragraph.");
-  });
+  // jQuery("p").click(function () {
+  //   alert("This is a paragraph.");
+  // });
 
-  jQuery("img").click(function () {
-    alert("This is an image.");
-  });
+  // jQuery("img").click(function () {
+  //   alert("This is an image.");
+  //   });
 });
+
+
